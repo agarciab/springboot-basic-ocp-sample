dad431543f054a9c292aa48f66f4c71b6380a833
Alexandre Garcia Boté <agarciab@viewnext.com>
2023-03-14 12:57:43 +0100

    Test controller

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+  @GetMapping("/{text}")
+  public String getText(@PathVariable String text) {
+    return "Hello " + text;
+  }
+
+}
